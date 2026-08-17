1f0a2ad0b8e678c2f292df40863ffdd0b1d06b72
Tom Schmidt <118602010+wocif@users.noreply.github.com>
2026-08-17 09:43:47 +0200

    feat: add Note domain model

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,11 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model
+
+import androidx.room.PrimaryKey
+
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {}
